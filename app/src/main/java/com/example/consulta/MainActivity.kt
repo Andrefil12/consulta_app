e262c95533015e72c36b1eefc0a9e1891a89bd54
Andrefil12 <andrefilipe-12@hotmail.com>
2021-04-08 18:37:54 -0300

    foi realizada a mudança de cor do app, criação da UI no MainActivity e adição de plugin

--- app/src/main/java/com/example/consulta/MainActivity.kt
+++ app/src/main/java/com/example/consulta/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.consulta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inputData()
+    }
+
+    private fun inputData(){
+
+        inpNome?.doAfterTextChanged {text ->
+            val nome = text.toString()
+        }
+
+        inpCpf?.doAfterTextChanged { text ->
+            val cpf = text.toString()
+        }
+
+        inpRg?.doAfterTextChanged { text ->
+            val rg = text.toString()
+        }
+
+        inpCell?.doAfterTextChanged { text ->
+            val cell = text.toString()
+        }
+
+
+    }
+
+    private fun callClass(nome: String, cpf: String, rg: String, cell: String){
+
     }
 }
